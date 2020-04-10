9dc6232cd8b507bf1015f955b83ad3addb339ad7
AlexisDominguez <alexisarieldominguezsanchez@pop-os.localdomain>
2020-04-10 15:37:08 -0600

    Register form validation added

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Icon, Form, Input } from "semantic-ui-react";
+import { validateEmail } from "../../../utils/Validations";
 import firebase from "../../../utils/Firebase";
 import "firebase/auth";
 
@@ -7,46 +8,102 @@ import "./RegisterForm.scss";
 
 export default function RegisterForm(props) {
   const { setSelectedForm } = props;
+  const [formData, setFormData] = useState(defaultValueForm());
+  const [showPassword, setShowPassword] = useState(false);
+  const [formError, setFormError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = () => {
-    console.log("Formulario enviado...");
+    setFormError({});
+    let errors = {};
+    let formOk = true;
+
+    if (!validateEmail(formData.email)) {
+      errors.email = true;
+      formOk = false;
+    }
+
+    if (formData.password.length < 6) {
+      errors.password = true;
+      formOk = false;
+    }
+
+    if (!formData.username) {
+      errors.username = true;
+      formOk = false;
+    }
+
+    setFormError(errors);
+
+    if (formOk) {
+      console.log("Formulario VALIDO");
+    }
+  };
+
+  const handlerShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <div className="register-form">
       <h1>Empieza a escuchar con una cuenta de Musicfy gratis</h1>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Field>
           <Input
             type="text"
             name="email"
             placeholder="Correo electrónico"
             icon="mail outline"
-            /* onChange="" */
-            /* error="" */
+            error={formError.email}
           ></Input>
+          {formError.email && (
+            <span className="error-text">
+              Por favor, introduce un correo electrónico válido.
+            </span>
+          )}
         </Form.Field>
-
         <Form.Field>
           <Input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Contraseña"
-            icon="eye"
-            /* onChange="" */
-            /* error="" */
+            icon={
+              showPassword ? (
+                <Icon
+                  name="eye slash outline"
+                  link
+                  onClick={handlerShowPassword}
+                />
+              ) : (
+                <Icon name="eye" link onClick={handlerShowPassword} />
+              )
+            }
+            error={formError.password}
           ></Input>
+          {formError.password && (
+            <span className="error-text">
+              La contraseña debe tener más de 6 caractéres.
+            </span>
+          )}
         </Form.Field>
-
         <Form.Field>
           <Input
             type="text"
             name="username"
             placeholder="¿Cómo deberíamos llamarte?"
             icon="user circle outline"
-            /* onChange="" */
-            /* error="" */
+            error={formError.username}
           ></Input>
+          {formError.username && (
+            <span className="error-text">Introduce un nombre de usuario.</span>
+          )}
         </Form.Field>
         <Button type="submit">Continuar</Button>
         <div className="register-form__options">
@@ -59,4 +116,12 @@ export default function RegisterForm(props) {
       </Form>
     </div>
   );
+}
+
+function defaultValueForm() {
+  return {
+    email: "",
+    password: "",
+    username: "",
+  };
 }
