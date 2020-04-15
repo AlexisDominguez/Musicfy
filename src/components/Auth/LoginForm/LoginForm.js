1296ead0ce33c1b5d8a74a557bb1a566d0f97e07
AlexisDominguez <alexisarieldominguezsanchez@pop-os.localdomain>
2020-04-14 21:14:15 -0600

    Login form frontend

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Icon, Form, Input } from "semantic-ui-react";
+import { toast } from "react-toastify";
+import { validateEmail } from "../../../utils/Validations";
+import firebase from "../../../utils/Firebase";
+import "firebase/auth";
 
-export default function LoginForm() {
+import "./LoginForm.scss";
+
+export default function LoginForm(props) {
+  const { setSelectedForm } = props;
+
+  const onSubmit = () => {
+    console.log("Login...");
+  };
+
   return (
-    <div>
-      <h1>LoginForm...</h1>
+    <div className="login-form">
+      <h1>Música para todos</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Correo electrónico"
+            icon="mail outline"
+            /* error="" */
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            icon="eye"
+            /* error="" */
+          />
+        </Form.Field>
+        <Button type="submit">Iniciar sesión</Button>
+      </Form>
+      <div className="login-form__options">
+        <p onClick={() => setSelectedForm(null)}>Volver</p>
+        <p>
+          ¿No tienes cuenta?{" "}
+          <span onClick={() => setSelectedForm("register")}>Regístrate</span>
+        </p>
+      </div>
     </div>
   );
 }
